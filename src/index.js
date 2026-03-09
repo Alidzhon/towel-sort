ce9be35a50272bcf78c55074c650f294a8393e15
Alidzhon <khosabekovalidzhon@gmail.com>
2026-03-09 17:20:22 +0300

    end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+  return matrix.reduce((acc, item, index) => {
+    return acc.concat(index % 2 === 0 ? item : item.reverse());
+  }, []);
 };
